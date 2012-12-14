f4b547516d71af341d5ee3d23262afc33c3555a7
Philip Luppens <philip.luppens@gmail.com>
2012-12-14 11:22:55 +0100

    Removed EP diffing implementation, merged diff_support branch.

--- src/main/java/org/nsesa/editor/gwt/an/server/service/gwt/GWTDiffServiceImpl.java
+++ src/main/java/org/nsesa/editor/gwt/an/server/service/gwt/GWTDiffServiceImpl.java
@@ -1,14 +1,10 @@
 package org.nsesa.editor.gwt.an.server.service.gwt;
 
-import ec.ep.diff4ep.service.AmendmentDiffingService;
-import ec.ep.diff4ep.service.model.DiffMethod;
 import org.nsesa.editor.gwt.core.client.service.gwt.GWTDiffService;
-import org.nsesa.editor.gwt.core.shared.ComplexDiffCommand;
-import org.nsesa.editor.gwt.core.shared.ComplexDiffContext;
-import org.nsesa.editor.gwt.core.shared.ComplexDiffResult;
+import org.nsesa.editor.gwt.core.shared.DiffRequest;
+import org.nsesa.editor.gwt.core.shared.DiffResult;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Date: 24/06/12 19:57
@@ -18,42 +14,19 @@ import java.util.List;
  */
 public class GWTDiffServiceImpl extends SpringRemoteServiceServlet implements GWTDiffService {
 
-    private AmendmentDiffingService amendmentDiffingService;
-
     @Override
-    public ArrayList<ComplexDiffResult> complexDiff(ArrayList<ComplexDiffCommand> commands) {
-        List<ec.ep.diff4ep.service.model.ComplexDiffCommand> epComplexDiffCommands = new ArrayList<ec.ep.diff4ep.service.model.ComplexDiffCommand>();
-        for (final ComplexDiffCommand complexDiffCommand : commands) {
-            final ComplexDiffContext context = complexDiffCommand.getContext();
-            final ec.ep.diff4ep.service.model.ComplexDiffContext epContext = new ec.ep.diff4ep.service.model.ComplexDiffContext(
-                    context.getOriginalChangeTemplate(), context.getOriginalComplexChangeTemplate(), context.getComplexInsertTemplate(), context.getComplexDeleteTemplate(),
-                    context.getComplexChangeTemplate(), DiffMethod.valueOf(context.getDiffMethod().toString())
-            );
+    public ArrayList<DiffResult> diff(final ArrayList<DiffRequest> diffRequests) {
 
-            ec.ep.diff4ep.service.model.ComplexDiffCommand epComplexDiffCommand = new ec.ep.diff4ep.service.model.ComplexDiffCommand(
-                    complexDiffCommand.getOriginal(), complexDiffCommand.getModified(), complexDiffCommand.getOverrideModified(), epContext);
-            epComplexDiffCommands.add(epComplexDiffCommand);
+        // NOTE: we don't actually do any diffing in here .. replace it with a diffing of your choice.
+        final ArrayList<DiffResult> results = new ArrayList<DiffResult>();
+        for (final DiffRequest diffRequest : diffRequests) {
+            results.add(new DiffResult(diffRequest.getOriginal(), diffRequest.getAmendment(), diffRequest.getDiffMethod()));
         }
-        final List<ec.ep.diff4ep.service.model.ComplexDiffResult> epComplexDiffResults = amendmentDiffingService.complexDiff(epComplexDiffCommands);
-
-        final ArrayList<ComplexDiffResult> diffResults = new ArrayList<ComplexDiffResult>();
-        for (ec.ep.diff4ep.service.model.ComplexDiffResult epComplexDiffResult : epComplexDiffResults) {
-            ComplexDiffResult result = new ComplexDiffResult(
-                    epComplexDiffResult.getOriginal(),
-                    epComplexDiffResult.getAmendment(),
-                    epComplexDiffResult.getTrackChangesOriginal(),
-                    epComplexDiffResult.getTrackChangesAmendment());
-            diffResults.add(result);
-        }
-        return diffResults;
+        return results;
     }
 
     @Override
     public String getVersion() {
-        return "test-0.0";
-    }
-
-    public void setAmendmentDiffingService(AmendmentDiffingService amendmentDiffingService) {
-        this.amendmentDiffingService = amendmentDiffingService;
+        return "no-diffing-0.0";
     }
 }
