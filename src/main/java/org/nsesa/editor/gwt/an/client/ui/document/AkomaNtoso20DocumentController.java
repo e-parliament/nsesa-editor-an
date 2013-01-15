206d433e268eff20ac27791b05323cc01f515139
Philip Luppens <philip.luppens@gmail.com>
2013-01-15 10:33:48 +0100

    Working on consolidation mode.

--- src/main/java/org/nsesa/editor/gwt/an/client/ui/document/AkomaNtoso20DocumentController.java
+++ src/main/java/org/nsesa/editor/gwt/an/client/ui/document/AkomaNtoso20DocumentController.java
@@ -6,8 +6,11 @@ import org.nsesa.editor.gwt.an.client.AkomaNtoso20DocumentInjector;
 import org.nsesa.editor.gwt.core.client.ClientFactory;
 import org.nsesa.editor.gwt.core.client.ServiceFactory;
 import org.nsesa.editor.gwt.core.client.diffing.DiffingManager;
+import org.nsesa.editor.gwt.core.client.mode.ActiveState;
+import org.nsesa.editor.gwt.core.client.mode.ConsolidationMode;
 import org.nsesa.editor.gwt.core.client.ui.overlay.Creator;
 import org.nsesa.editor.gwt.core.client.ui.overlay.Locator;
+import org.nsesa.editor.gwt.core.client.ui.overlay.document.AmendableWidget;
 import org.nsesa.editor.gwt.core.client.ui.overlay.document.OverlayFactory;
 import org.nsesa.editor.gwt.editor.client.ui.document.DocumentController;
 import org.nsesa.editor.gwt.editor.client.ui.document.DocumentInjector;
@@ -24,6 +27,23 @@ public class AkomaNtoso20DocumentController extends DocumentController {
     @Inject
     public AkomaNtoso20DocumentController(ClientFactory clientFactory, ServiceFactory serviceFactory, OverlayFactory overlayFactory, DiffingManager diffingManager, Locator locator, Creator creator, InlineEditorController inlineEditorController) {
         super(clientFactory, serviceFactory, overlayFactory, diffingManager, locator, creator, inlineEditorController);
+        registerMode(ConsolidationMode.KEY, new ConsolidationMode(this, clientFactory) {
+            @Override
+            public boolean apply(ActiveState state) {
+                if (state.isActive()) {
+                    walk(new AmendableVisitor() {
+                        @Override
+                        public boolean visit(final AmendableWidget visited) {
+                            /*if (visited.isAmended()) {
+                                visited.asWidget().getElement().getStyle().setBackgroundColor("blue");
+                            }*/
+                            return true;
+                        }
+                    });
+                }
+                return super.apply(state);
+            }
+        });
     }
 
     @Override
