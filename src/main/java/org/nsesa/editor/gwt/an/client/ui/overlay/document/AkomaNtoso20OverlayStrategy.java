749865d895acbe0189978024a85cd0a81de8b6ce
Philip Luppens <philip.luppens@gmail.com>
2013-01-21 13:40:09 +0100

    Refactoring of the overlay strategy (made DefaultOverlayStrategy a bit smarter) + added some first tests.

--- src/main/java/org/nsesa/editor/gwt/an/client/ui/overlay/document/AkomaNtoso20OverlayStrategy.java
+++ src/main/java/org/nsesa/editor/gwt/an/client/ui/overlay/document/AkomaNtoso20OverlayStrategy.java
@@ -1,13 +1,6 @@
 package org.nsesa.editor.gwt.an.client.ui.overlay.document;
 
-import com.google.gwt.dom.client.Element;
-import org.nsesa.editor.gwt.core.client.ui.overlay.Format;
-import org.nsesa.editor.gwt.core.client.ui.overlay.NumberingType;
-import org.nsesa.editor.gwt.core.client.ui.overlay.document.OverlayStrategy;
-import org.nsesa.editor.gwt.core.client.ui.overlay.document.OverlayStrategySupport;
-
-import java.util.Arrays;
-import java.util.List;
+import org.nsesa.editor.gwt.core.client.ui.overlay.document.DefaultOverlayStrategy;
 
 /**
  * Date: 03/07/12 22:54
@@ -15,79 +8,8 @@ import java.util.List;
  * @author <a href="philip.luppens@gmail.com">Philip Luppens</a>
  * @version $Id$
  */
-public class AkomaNtoso20OverlayStrategy implements OverlayStrategy {
-
-    private OverlayStrategySupport overlayStrategySupport = new OverlayStrategySupport();
+public class AkomaNtoso20OverlayStrategy extends DefaultOverlayStrategy {
 
     public AkomaNtoso20OverlayStrategy() {
-        //overlayStrategySupport.asProperties(Num.class, P.class, B.class, I.class, Span.class, Content.class);
-    }
-
-    @Override
-    public String getSource(Element element) {
-        return overlayStrategySupport.getSource(element);
-    }
-
-    @Override
-    public String getID(Element element) {
-        return overlayStrategySupport.getID(element);
-    }
-
-    @Override
-    public Boolean isAmendable(Element element) {
-        if (!Arrays.asList("num", "p", "b", "i", "span", "content").contains(element.getAttribute("type").toLowerCase()))
-            return overlayStrategySupport.isAmendable(element);
-        return false;
-    }
-
-    @Override
-    public Boolean isImmutable(Element element) {
-        return overlayStrategySupport.isImmutable(element);
-    }
-
-    @Override
-    public String getType(Element element) {
-        return overlayStrategySupport.getType(element);
-    }
-
-    @Override
-    public String getNamespaceURI(Element element) {
-        return overlayStrategySupport.getNamespaceURI(element);
-    }
-
-    @Override
-    public String getIndex(Element element) {
-        return overlayStrategySupport.getLiteralIndex(element);
-    }
-
-    @Override
-    public NumberingType getNumberingType(Element element, int index) {
-        return overlayStrategySupport.getNumberingType(element, index);
-    }
-
-    @Override
-    public Format getFormat(Element element) {
-        return overlayStrategySupport.getFormat(element);
-    }
-
-    @Override
-    public String getInnerHTML(Element element) {
-        return overlayStrategySupport.getInnerHTML(element);
-    }
-
-    @Override
-    public Element[] getChildren(Element element) {
-        final List<Element> children = overlayStrategySupport.getChildren(element);
-        return children.toArray(new Element[children.size()]);
-    }
-
-    @Override
-    public String getFormattedIndex(Element element) {
-        return overlayStrategySupport.getLiteralIndex(element);
-    }
-
-    @Override
-    public String getUnFormattedIndex(Element element) {
-        return overlayStrategySupport.getUnformattedIndex(element);
     }
 }
