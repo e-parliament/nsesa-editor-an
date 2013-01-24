3f9678c83efd2f82e159309162108054e55fb1f0
Stelian Groza <stelian.groza@gmail.com>
2013-01-24 20:55:20 +0100

    #75 Drafting support on amendment dialog improve the drafting tool functionality

--- web/js/ckeditor/plugins/nsesa/plugin.js
+++ web/js/ckeditor/plugins/nsesa/plugin.js
@@ -14,7 +14,23 @@
             },
             init : function( editor )
             {
+                var buttonName = "NsesaToggle";
+                var cmd = {
+                    exec: function(editor) {
+                        editor.getCommand(buttonName).toggleState();
+                        var nsesaState = editor.getCommand(buttonName).state;
+                        var nsesaToggle = {nsesaToggleDraft : (nsesaState == CKEDITOR.TRISTATE_ON)};
+                        editor.fire("nsesaToggleDraft", nsesaToggle);
+                    }
+                }
+                editor.addCommand(buttonName, cmd);
+                editor.ui.addButton(buttonName,{
+                    label:"Toggle Drafting tool",
+                    icon:this.path + "nsesaDraftingTool.png",
+                    command: buttonName
+                });
                 CKEDITOR.nsesaInit(editor);
+
             }
         });
 
