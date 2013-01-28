ee865644f9a49ea2178e45914a4e0ad45ed1c57d
Stelian Groza <stelian.groza@gmail.com>
2013-01-28 15:34:51 +0100

    #77 expose comments from xsd in the generated classes - create a new OverlayLocalizableResource class that handle this functionality

---
+++ src/main/java/org/nsesa/editor/gwt/an/client/ui/overlay/document/gen/NamespaceOverlayLocalizableResource.java
@@ -0,0 +1,36 @@
+package org.nsesa.editor.gwt.an.client.ui.overlay.document.gen;
+
+import com.google.inject.Inject;
+import com.google.gwt.core.client.GWT;
+import org.nsesa.editor.gwt.core.client.ui.overlay.document.AmendableWidget;
+import org.nsesa.editor.gwt.core.client.ui.overlay.document.DefaultOverlayLocalizableResource;
+import com.google.gwt.i18n.client.Messages;
+import java.util.logging.Logger;
+import java.util.Map;
+/**
+* Note: this file is generated. Rather than changing this file, correct the template called <tt>overlayLocalizableResource.ftl</tt>.
+*/
+public class NamespaceOverlayLocalizableResource extends DefaultOverlayLocalizableResource {
+    private static NamespaceOverlayMessages MESSAGES = GWT.create(NamespaceOverlayMessages.class);
+
+    @Override
+    public String getName(final AmendableWidget widget) {
+        if (widget == null) {
+            throw new IllegalArgumentException("Null widget passed.");
+        }
+        else {
+            return widget.getType();
+        }
+    }
+
+    @Override
+    public String getDescription(final AmendableWidget widget) {
+        if (widget == null) {
+            throw new IllegalArgumentException("Null widget passed.");
+        }
+        else {
+            return widget.getType();
+        }
+    }
+
+}
---
+++ src/main/java/org/nsesa/editor/gwt/an/client/ui/overlay/document/gen/XmlschemaOverlayMessages.java
@@ -0,0 +1,11 @@
+
+package org.nsesa.editor.gwt.an.client.ui.overlay.document.gen;
+
+import com.google.gwt.i18n.client.Messages;
+
+/**
+* Note: this file is generated. Rather than changing this file, correct the template called <tt>overlayMessages.ftl</tt>.
+*/
+
+public interface XmlschemaOverlayMessages extends Messages {
+}
