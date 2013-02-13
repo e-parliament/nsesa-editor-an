99f768adb9e51b2dd11263bf99f629ab10eca8ca
Philip Luppens <philip.luppens@gmail.com>
2013-02-13 10:51:29 +0100

    Issue #33: added selenium dependencies.

---
+++ src/test/integration/org/nsesa/editor/gwt/an/it/SeleniumIT.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2013 European Parliament
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+package org.nsesa.editor.gwt.an.it;
+
+import junit.framework.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Date: 13/02/13 10:31
+ *
+ * @author <a href="philip.luppens@gmail.com">Philip Luppens</a>
+ * @version $Id$
+ */
+public class SeleniumIT {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://localhost:8080/editor/editor.html?documentID=1");
+        final String title = driver.getTitle();
+        new WebDriverWait(driver, 10).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver input) {
+                return input.getTitle().toLowerCase().contains("document 1");
+            }
+        });
+        Assert.assertEquals("Document 1 - Nsesa Editor", driver.getTitle());
+        driver.quit();
+    }
+}
