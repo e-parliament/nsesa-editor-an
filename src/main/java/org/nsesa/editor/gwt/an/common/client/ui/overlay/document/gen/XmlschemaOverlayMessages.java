de77ca5e6d5b93b9a5efed3b007c765e63f5bfdc
Philip Luppens <philip.luppens@gmail.com>
2013-04-10 10:28:27 +0200

    Fixed bad characters in UTF-8 header causing problems in the license plugin.

--- src/main/java/org/nsesa/editor/gwt/an/common/client/ui/overlay/document/gen/XmlschemaOverlayMessages.java
+++ src/main/java/org/nsesa/editor/gwt/an/common/client/ui/overlay/document/gen/XmlschemaOverlayMessages.java
@@ -1,7 +1,7 @@
 /**
  * Copyright 2013 European Parliament
  *
- * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
  *
@@ -11,7 +11,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Licence for the specific language governing permissions and limitations under the Licence.
  */
-
 package org.nsesa.editor.gwt.an.common.client.ui.overlay.document.gen;
 
 import com.google.gwt.i18n.client.Messages;
---
+++ src/main/java/org/nsesa/editor/gwt/an/drafting/client/ui/main/document/sourcefile/actionbar/StaticActionBarController.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2013 European Parliament
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+package org.nsesa.editor.gwt.an.drafting.client.ui.main.document.sourcefile.actionbar;
+
+import com.google.inject.Inject;
+import org.nsesa.editor.gwt.core.client.ui.document.DocumentEventBus;
+import org.nsesa.editor.gwt.core.client.ui.document.sourcefile.actionbar.ActionBarController;
+import org.nsesa.editor.gwt.core.client.ui.document.sourcefile.actionbar.ActionBarView;
+import org.nsesa.editor.gwt.core.client.ui.document.sourcefile.actionbar.ActionBarViewCss;
+import org.nsesa.editor.gwt.core.client.ui.document.sourcefile.actionbar.create.ActionBarCreatePanelController;
+
+/**
+ * Date: 10/04/13 09:57
+ *
+ * @author <a href="philip.luppens@gmail.com">Philip Luppens</a>
+ * @version $Id$
+ */
+public class StaticActionBarController extends ActionBarController {
+
+    @Inject
+    public StaticActionBarController(final DocumentEventBus documentEventBus,
+                                     final ActionBarView view,
+                                     final ActionBarViewCss actionBarViewCss,
+                                     final ActionBarCreatePanelController actionBarCreatePanelController) {
+        super(documentEventBus, view, actionBarViewCss, actionBarCreatePanelController);
+    }
+
+    @Override
+    protected void onScroll() {
+        // doesn't do anything
+    }
+}
