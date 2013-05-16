13d829537097a8d3a741d324d66d8f9561e66923
Philip Luppens <philip.luppens@gmail.com>
2013-05-16 17:36:50 +0200

    Added history link in the action panel.

--- src/main/java/org/nsesa/editor/gwt/an/amendments/client/ui/amendment/action/AkomaNtoso20AmendmentActionPanelController.java
+++ src/main/java/org/nsesa/editor/gwt/an/amendments/client/ui/amendment/action/AkomaNtoso20AmendmentActionPanelController.java
@@ -17,13 +17,26 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
 import com.google.inject.Inject;
+import org.nsesa.editor.gwt.amendment.client.event.amendment.AmendmentContainerSaveEvent;
 import org.nsesa.editor.gwt.amendment.client.ui.amendment.action.AmendmentActionPanelController;
 import org.nsesa.editor.gwt.amendment.client.ui.amendment.action.AmendmentActionPanelView;
+import org.nsesa.editor.gwt.compare.client.event.ShowComparePanelEvent;
+import org.nsesa.editor.gwt.compare.client.ui.compare.ComparisonProvider;
+import org.nsesa.editor.gwt.core.client.ClientFactory;
+import org.nsesa.editor.gwt.core.client.ServiceFactory;
+import org.nsesa.editor.gwt.core.client.event.CriticalErrorEvent;
 import org.nsesa.editor.gwt.core.client.ui.i18n.CoreMessages;
-import org.nsesa.editor.gwt.core.client.ui.overlay.Transformer;
+import org.nsesa.editor.gwt.core.shared.AmendmentContainerDTO;
+import org.nsesa.editor.gwt.core.shared.RevisionDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Date: 06/02/13 16:19
@@ -32,8 +45,6 @@ import org.nsesa.editor.gwt.core.client.ui.overlay.Transformer;
  * @version $Id$
  */
 public class AkomaNtoso20AmendmentActionPanelController extends AmendmentActionPanelController {
-
-    private final Transformer transformer;
 
     private final PopupPanel exportPopupPanel = new DecoratedPopupPanel(false, false) {
         {
@@ -47,11 +58,9 @@ public class AkomaNtoso20AmendmentActionPanelController extends AmendmentActionP
     private final HTML content = new HTML();
 
     @Inject
-    public AkomaNtoso20AmendmentActionPanelController(AmendmentActionPanelView amendmentActionPanelView,
-                                                      final CoreMessages coreMessages,
-                                                      final Transformer transformer) {
+    public AkomaNtoso20AmendmentActionPanelController(final AmendmentActionPanelView amendmentActionPanelView,
+                                                      final CoreMessages coreMessages) {
         super(amendmentActionPanelView, coreMessages);
-        this.transformer = transformer;
         this.exportPopupPanel.setWidget(mainPanel);
 
         this.mainPanel.setHeight("600px");
@@ -74,6 +83,76 @@ public class AkomaNtoso20AmendmentActionPanelController extends AmendmentActionP
             }
         });
 
+        final Anchor history = new Anchor("History");
+        history.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+
+                final ServiceFactory serviceFactory = amendmentController.getDocumentController().getServiceFactory();
+                final ClientFactory clientFactory = amendmentController.getDocumentController().getClientFactory();
+
+                final ComparisonProvider comparisonProvider = new ComparisonProvider() {
+                    @Override
+                    public void getRevisionContent(final String revisionID, final AsyncCallback<String> asyncCallback) {
+                        serviceFactory.getGwtAmendmentService().getAmendmentContainerRevision(clientFactory.getClientContext(), revisionID, new AsyncCallback<AmendmentContainerDTO>() {
+                            @Override
+                            public void onFailure(final Throwable caught) {
+                                asyncCallback.onFailure(caught);
+                            }
+
+                            @Override
+                            public void onSuccess(final AmendmentContainerDTO result) {
+                                // wrap in a span to correct the rendering via amendment.css
+                                final Element span = DOM.createSpan();
+                                span.setInnerHTML(result.getBody());
+                                final String body = "<div class='amendmentHolder'><div>" + span.getFirstChildElement() + "</div></div>";
+                                asyncCallback.onSuccess(body);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void getRevisions(final AsyncCallback<List<RevisionDTO>> asyncCallback) {
+                        serviceFactory.getGwtAmendmentService().getRevisions(clientFactory.getClientContext(), amendmentController.getModel().getId(), new AsyncCallback<ArrayList<RevisionDTO>>() {
+                            @Override
+                            public void onFailure(final Throwable caught) {
+                                asyncCallback.onFailure(caught);
+                            }
+
+                            @Override
+                            public void onSuccess(final ArrayList<RevisionDTO> result) {
+                                asyncCallback.onSuccess(result);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void rollback(final String revisionID) {
+                        serviceFactory.getGwtAmendmentService().getAmendmentContainerRevision(clientFactory.getClientContext(), revisionID, new AsyncCallback<AmendmentContainerDTO>() {
+                            @Override
+                            public void onFailure(Throwable caught) {
+                                clientFactory.getEventBus().fireEvent(new CriticalErrorEvent("Could not rollback revision: " + caught.getMessage(), caught));
+                            }
+
+                            @Override
+                            public void onSuccess(AmendmentContainerDTO result) {
+                                // force an update of the revision ID to let the backend know we're aware of the latest version
+                                result.setRevisionID(amendmentController.getModel().getRevisionID());
+                                amendmentController.getDocumentController().getDocumentEventBus().fireEvent(new AmendmentContainerSaveEvent(result));
+                                popupPanel.hide();
+                            }
+                        });
+                    }
+                };
+
+                amendmentController.getDocumentController().getClientFactory().getEventBus().fireEvent(new ShowComparePanelEvent(amendmentController.getDocumentController(), comparisonProvider));
+                popupPanel.hide();
+            }
+        });
+//        TODO not finished yet (needs extraction of the amendment text).
+        addSeparator();
+        addWidget(history);
+
         final Anchor xmlExport = new Anchor("Export to XML");
         xmlExport.addClickHandler(new ClickHandler() {
             @Override
@@ -81,7 +160,7 @@ public class AkomaNtoso20AmendmentActionPanelController extends AmendmentActionP
                 final String url = GWT.getModuleBaseURL() + "download/amendment/xml/" + amendmentController.getModel().getId();
                 Window.open(URL.encode(url), "download", "");
                 popupPanel.hide();
-            };
+            }
         });
         final Anchor pdfExport = new Anchor("Export to PDF");
         pdfExport.addClickHandler(new ClickHandler() {
