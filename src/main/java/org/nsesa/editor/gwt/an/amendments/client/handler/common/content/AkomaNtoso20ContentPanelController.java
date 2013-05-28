333992647490e83a60f19d9d486052b32bf9c41b
Philip Luppens <philip.luppens@gmail.com>
2013-05-28 16:32:00 +0200

    Incorrect serialization of the original content was resulting in the element's parent tag being added over and over again.

--- src/main/java/org/nsesa/editor/gwt/an/amendments/client/handler/common/content/AkomaNtoso20ContentPanelController.java
+++ src/main/java/org/nsesa/editor/gwt/an/amendments/client/handler/common/content/AkomaNtoso20ContentPanelController.java
@@ -16,6 +16,7 @@ package org.nsesa.editor.gwt.an.amendments.client.handler.common.content;
 import com.google.inject.Inject;
 import org.nsesa.editor.gwt.an.amendments.client.ui.amendment.AkomaNtoso20AmendmentControllerUtil;
 import org.nsesa.editor.gwt.core.client.ClientFactory;
+import org.nsesa.editor.gwt.core.client.ui.overlay.document.OverlayWidget;
 import org.nsesa.editor.gwt.dialog.client.ui.dialog.DialogContext;
 import org.nsesa.editor.gwt.dialog.client.ui.handler.common.content.ContentControllerView;
 import org.nsesa.editor.gwt.dialog.client.ui.handler.common.content.ContentPanelController;
@@ -42,7 +43,10 @@ public class AkomaNtoso20ContentPanelController extends ContentPanelController {
 
         if (dialogContext.getAmendmentController() != null) {
             // we're editing
-            view.setOriginalText(AkomaNtoso20AmendmentControllerUtil.getOriginalContentFromModel(dialogContext.getAmendmentController()));
+            final OverlayWidget overlayWidget = dialogContext.getAmendmentController().asAmendableWidget(
+                    AkomaNtoso20AmendmentControllerUtil.getOriginalContentFromModel(
+                            dialogContext.getAmendmentController()));
+            view.setOriginalText(overlayWidget.getInnerHTML());
         } else {
             view.setOriginalText(dialogContext.getOverlayWidget().getInnerHTML());
         }
