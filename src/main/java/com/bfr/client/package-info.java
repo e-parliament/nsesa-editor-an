f6f896b51d8fd04b3cf6cd1391ac8a87d0991f12
Philip Luppens <philip.luppens@gmail.com>
2013-07-25 11:37:04 +0200

    Added package info for the imported gwt-selection code.

---
+++ src/main/java/com/bfr/client/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Note: this package is imported from the gwt-selection project because it is not available in Maven,
+ * and including jars in our repository, as well as extending the build to include this jar is far more
+ * than I'm willing to do. So, until this jar shows up (mail was sent with the request on the 18th of July 2013),
+ * we're going to simply include it here.
+ *
+ * @see <a href="https://code.google.com/p/gwt-selection/">https://code.google.com/p/gwt-selection/</a>
+ */
+package com.bfr.client;
